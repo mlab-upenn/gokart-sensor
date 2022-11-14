667361d01e56dfa6cddb11d6c0dfd2f0c00ed72f
Felix Jahncke <fjahncke@live.de>
2022-11-14 16:23:20 -0500

    Change to correct color space

--- catkin_LiCaFusion/src/lidar_rings/src/licafusion.py
+++ catkin_LiCaFusion/src/lidar_rings/src/licafusion.py
@@ -33,13 +33,9 @@ def project_points(projection_matrix, points):
     return uvs.T
 
 def callback(image, pcl, bb):
-    # rospy.loginfo("1")
-    pcl_XYZIR = pcl
     xyz_arr = ros_numpy.point_cloud2.pointcloud2_to_xyz_array(pcl)
-    # print(xyz_arr.shape)
     point_arr = np.ones((xyz_arr.shape[0]))
     h_array = np.column_stack((xyz_arr, point_arr))
-    # rospy.loginfo("%s", pcl_XYZIR.fields)
     c_t_l = np.array([[0.0190983 , -0.999815 ,0.00232702 , 0.0440449],
                     [-0.193816 ,-0.0059855 ,  -0.98102 ,  0.142687],
                     [ 0.980852 , 0.0182848 , -0.193894 ,-0.0378563],
@@ -47,11 +43,8 @@ def callback(image, pcl, bb):
     proj_mat = np.array([[809.148438 , 0.0 ,328.785593 , 0.0],
                     [0.0 ,808.892273 ,  239.680971 ,  0.0],
                     [ 0.0 , 0.0 , 1.0 ,0.0]])
-    # l_t_c = np.linalg.inv(c_t_l)
     cam_points = c_t_l@h_array.T
-    # print(cam_points[:,0])
     uvs = project_points(proj_mat, cam_points.T)
-    # print(uvs[:4, :])
 
     point_in_fovx = np.bitwise_and([uvs[:,0] >= 0], [(uvs[:,0]) < image.width])
     point_in_fovy = np.bitwise_and([uvs[:,1] >= 0], [(uvs[:,1]) < image.height])
@@ -59,9 +52,7 @@ def callback(image, pcl, bb):
     uvs = uvs[np.squeeze(point_in_fov),:]
     depth = h_array[np.squeeze(point_in_fov),:]
 
-    # print(image.width)
-    # print(image.height)
-    # print(uvs.shape)
+
     color_red = (0, 0, 255)
     fields = [PointField('x', 0, PointField.FLOAT32, 1),
           PointField('y', 4, PointField.FLOAT32, 1),
@@ -70,7 +61,6 @@ def callback(image, pcl, bb):
 
     cv_img = bridge.imgmsg_to_cv2(image, "passthrough")
     bb_arr = bb.bounding_boxes
-    # depth_bb_coll = np.array([])
     j=0
     for bs in bb_arr:
         point_in_bbx = np.bitwise_and([uvs[:,0] >= bs.xmin], [(uvs[:,0]) < bs.xmax])
@@ -78,7 +68,6 @@ def callback(image, pcl, bb):
         point_in_bb = np.bitwise_and(point_in_bbx, point_in_bby)
         uvs_bb = uvs[np.squeeze(point_in_bb),:]
         depth_bb = depth[np.squeeze(point_in_bb),:]
-        # print(depth_bb.shape)
         if (j==0):
             depth_bb_coll = depth_bb
             j+=1
@@ -93,7 +82,7 @@ def callback(image, pcl, bb):
     pc = point_cloud2.create_cloud(header, fields, depth_bb_coll[:,:3])
     pub.publish(pc)
 
-    cv_img = bridge.cv2_to_imgmsg(cv_img, "passthrough")
+    cv_img = bridge.cv2_to_imgmsg(cv_img, "rgb8")
     pub2.publish(cv_img)
 
 
